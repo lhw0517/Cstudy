b94e40e1fb372dba6617e82c9a646565de154233
lhw0517 <Hrotonstudio@163.com>
2023-07-13 16:42:59 +0800

    1

--- Cstudy/1.c
+++ Cstudy/1.c
@@ -1,6 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 int main()
 {
-	printf("你好");
+	printf("你好啊\n");
+	//   加法计算
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	printf("小加法\n");
+	printf("输入两个数:  ");
+	scanf("%d %d", &a, &b);   //scanf输入函数
+	sum = a + b; //求a+b的和
+	printf("和 = %d\n", sum);
 	return 0;
 }
