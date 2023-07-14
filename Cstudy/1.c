3806b2890f8c0387e39c5199dbed038af2f271ad
lhw0517 <Hrotonstudio@163.com>
2023-07-14 20:46:34 +0800

    1

--- Cstudy/1.c
+++ Cstudy/1.c
@@ -12,5 +12,28 @@ int main()
 	scanf("%d %d", &a, &b);   //scanf输入函数
 	sum = a + b; //求a+b的和
 	printf("和 = %d\n", sum);
+
+	printf("%d\n", 100);
+	printf("%c\n", 't');
+	printf("%s\n", "我是字符串");
+
+	// 简单的if语句 :)
+	printf("输入1 不要输入0 : ");
+	int input = 0;
+	scanf("%d", &input);
+	if (input == 1)
+	{
+		printf("太棒了没输入0");
+	}
+	else
+	{
+		printf("为什么不输入1啊！！！");
+	}
+	/*
+	* 多行注释
+	* yeeeeee
+	*/
+
+	// 单行注释
 	return 0;
 }
